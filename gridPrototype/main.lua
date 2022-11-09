efc4a27bec16a71f50a00b43052cc5c9a3a805f5
Gavin Thomson <98289315+glamourmuscle@users.noreply.github.com>
2022-11-09 00:12:26 +0000

    possible moves hightlight
    not entirely working

--- gridPrototype/main.lua
+++ gridPrototype/main.lua
@@ -1,89 +1,105 @@
 function love.load()
-listOfRectangles = {}
-game = {}
-window = {}
-window.width = 1024
-window.height = 768
-game.width = window.width * 0.9
-game.height = window.height * 0.9
-love.window.setMode(window.width, window.height)
-for i=0, 9
-do
-	for j = 0,9 do
-
-	createRect(i*(game.width/10) + (game.width/20), j*(game.height/10) + game.width/20, i, j)
-	end
-
-
-end
-
-
-
+		listOfRectangles = {}
+		currentLocation = {2,7}
+		game = {}
+		window = {}
+		window.width = 1024
+		window.height = 768
+		game.width = window.width * 0.9
+		game.height = window.height * 0.9
+		game.gridx = 9
+		game.gridy = 9
+		love.window.setMode(window.width, window.height)
+		for i=0, 9
+			do
+				for j = 0,9 do
+				createRect(i*(game.width/10) + (game.width/20), j*(game.height/10) + game.width/20, i, j)
+				end
+			end
 end
 
 
 function createRect(xpos, ypos, xgrid, ygrid)
-rect = {}
-rect.x = xpos
-rect.y = ypos
-rect.width = (game.width/10 - 2)
-rect.height = (game.height/10 -2)
-rect.speed = 0
---rect.red = 255
---rect.green = 255
---rect.blue = 255
-rect.color = {0, 0, 0}
-rect.clicked = 0
-rect.mode = "line"
-rect.xgrid = xgrid
-rect.ygrid = ygrid
-table.insert(listOfRectangles, rect)
-
+		rect = {}
+		rect.x = xpos
+		rect.y = ypos
+		rect.width = (game.width/10 - 2)
+		rect.height = (game.height/10 -2)
+		rect.color = {0, 0, 0}
+		rect.clicked = 0
+		rect.mode = "line"
+		rect.xgrid = xgrid
+		rect.ygrid = ygrid
+		rect.status = 0
+		table.insert(listOfRectangles, rect)
 end
 
 function love.update(dt)
 
+end
 
+function love.mousepressed(x, y, button, istouch)
+
+  if button == 1 then
+
+		for i,v in ipairs(listOfRectangles) do
+				if
+						x >= (v.x) and x <= (v.x+(game.width/10))
+						and
+						y >= v.y
+						and
+						y <= (v.y+(game.height/10))
+						and
+						v.xgrid == currentLocation[1]
+						and
+						v.ygrid == currentLocation[2]
+				then
+					currentPressed(i)
+					else if v.status == 2 then
+						newPressed(i)
+					end
+				end
+		end
+	end
+
+	if button == 2 then
+		for i,v in ipairs(listOfRectangles) do
+			v.clicked =0
+			v.mode = "line"
+		end
+	end
 
 end
 
 
+function currentPressed(rectNum)
 
+	listOfRectangles[rectNum].status = 1
+	currentxgrid = listOfRectangles[rectNum].xgrid
+	currentygrid = listOfRectangles[rectNum].ygrid
 
+	for i,v in ipairs(listOfRectangles) do
+		if
+		(((v.xgrid-currentxgrid == 1 or v.xgrid-currentxgrid == -1)
+		and v.xgrid > -1
+		and v.xgrid < game.gridx + 1)
+		and v.ygrid == currentygrid)
+		or
+		(((v.ygrid-currentygrid == 1 or v.ygrid-currentygrid == -1)
+		and v.ygrid > -1
+		and v.ygrid < game.gridy + 1)
+		and v.xgrid == currentxgrid)
 
-function love.mousepressed(x, y, button, istouch)
-
-    if button == 1 then
-       
-		for i,v in ipairs(listOfRectangles) do
-
-			if x >= (v.x) and x <= (v.x+(game.width/10))
-				and
-				y >= v.y
-				and
-				y <= (v.y+(game.height/10))
-				then
-
-				v.mode = "fill"
-				v.clicked = 1
-
-			end
-
-
-    	end
-	end
-
-	if button == 2 then
-
-		for i,v in ipairs(listOfRectangles) do
-			--v.color = {255,255,255}
-			v.clicked =0
-			v.mode = "line"
-			
-
+		then
+			v.status = 2
 		end
-	end	
+	end
+end
 
+function newPressed(rectNum)
+	listOfRectangles[rectNum].status =1
+	currentLocation[1] = listOfRectangles[rectNum].xpos
+	currentLocation[2] = listOfRectangles[rectNum].ypos
 end
 
 
@@ -91,24 +107,28 @@ function love.draw()
 love.graphics.setColor(1,1,1)
 love.graphics.rectangle("fill", 0, 0, window.width, window.height)
 
+	for i,v in ipairs(listOfRectangles) do
 
+		if v.xgrid == currentLocation[1] and v.ygrid == currentLocation[2] then
+			v.status = 1
+			v.mode = "fill"
+		end
 
+				if v.status == 0 then
+					love.graphics.setColor(v.color)
+				end
 
-for i,v in ipairs(listOfRectangles) do
-	love.graphics.setColor(rect.color)
-	love.graphics.rectangle(v.mode, v.x, v.y, v.width, v.height)
+				if v.status == 1 then
+					love.graphics.setColor(0, 255, 0)
+				end
 
+				if v.status == 2 then
+					love.graphics.setColor(0,255,255)
+				end
 
-	love.graphics.print(string.char(v.xgrid+65) .. v.ygrid+1, v.x+(game.width/20), v.y+game.height/20)
+		love.graphics.rectangle(v.mode, v.x, v.y, v.width, v.height)
+		love.graphics.print(string.char(v.xgrid+65) .. v.ygrid+1, v.x+(game.width/20), v.y+game.height/20)
 
+	end
 
 end
-
-
-
-end
-
-
-
-
-
